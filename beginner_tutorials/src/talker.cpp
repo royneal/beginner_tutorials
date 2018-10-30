bb43ffe3c394c90d1e78fbd8ea5e1b7305671185
royneal <royneal@gmail.com>
2018-10-30 12:57:25 -0400

    added custom message to talker.cpp

--- beginner_tutorials/src/talker.cpp
+++ beginner_tutorials/src/talker.cpp
@@ -61,7 +61,7 @@ int main(int argc, char **argv)
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "hello world " << count;
+    ss << "the journey of 1000 miles begins with a single step" << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
