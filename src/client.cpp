e2ce0724713bc931a9495ae4a4c973f26ae80bf7
royneal <royneal@gmail.com>
2018-11-06 16:28:41 -0500

    added command line arguments to client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -13,20 +13,21 @@
 
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "changerate");
-  /*if (argc != 3) {
+  ros::init(argc, argv, "client_node");
+  if (argc != 2) {
     ROS_ERROR("Error: no imput arguments  ");
-    return 1;
-  }*/
+    return -1;
+  }
 
   ros::NodeHandle nh;
   ros::ServiceClient client = nh.serviceClient<beginner_tutorials::message_rate>
   ("message_rate");
   beginner_tutorials::message_rate srv;
-  srv.request.rate = 15; 
+  srv.request.rate = std::stoi(argv[1]); 
  
   if (client.call(srv)) {
     ROS_INFO_STREAM("Old Message Rate: "<< srv.response.oldrate);
+    ROS_INFO_STREAM("New Message Rate: "<< srv.request.rate);
   } else {
     ROS_FATAL("Failed to call service message_rate");
     return 1;
