9cd17554d4c1db9a5a6677ef35b1c4e44f387315
royneal <royneal@gmail.com>
2018-11-06 18:27:36 -0500

    fixed cpplint errors

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -11,8 +11,7 @@
 #include "beginner_tutorials/publisher.h"
 #include "beginner_tutorials/message_rate.h"
 
- int Publisher::msg_rate_=1;
- 
+int Publisher::msg_rate_ = 1;
 /**
  * @brief      default class constructor, advertises the chatter topic
  */
@@ -22,10 +21,10 @@ Publisher::Publisher() {
 
 /**
  * @brief      Service call back function, loads new publising rate
- * @param      req holds service request 
- * @param      resp holds service response 
+ * @param      req holds service request
+ * @param      resp holds service response
  */
-bool Publisher::SetRate(beginner_tutorials::message_rate::Request & req, 
+bool Publisher::SetRate(beginner_tutorials::message_rate::Request & req,
                     beginner_tutorials::message_rate::Response &resp ) {
      resp.oldrate = msg_rate_;
      msg_rate_ = req.rate;
@@ -37,21 +36,18 @@ bool Publisher::SetRate(beginner_tutorials::message_rate::Request & req,
  * @param      rate holds new publishing rate requested by client
  *  
  */
-void Publisher::SettxRate(int& rate){
+void Publisher::SettxRate(const int& rate) {
     msg_rate_ = rate;
-
 }
 
 /**
- * @brief      method to publis messages on chatter topic 
- * @param      msg string to be published on topic 
+ * @brief      method to publis messages on chatter topic
+ * @param      msg string to be published on topic
  */
 void Publisher::Publish(const std::string& msg) {
-
-    
     ros::Rate loop_rate(msg_rate_);  // rate at which messages get published
     std_msgs::String ros_msg;  // standard ros message type string
-    service_=h_.advertiseService("message_rate",& Publisher::SetRate);
+    service_ = h_.advertiseService("message_rate", & Publisher::SetRate);
 
     while (ros::ok()) {
         ros::Rate loop_rate(msg_rate_);  // rate at which messages get published
