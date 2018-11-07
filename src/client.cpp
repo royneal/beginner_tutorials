76de8b5ad701d44ac1bf6386b3f53add86c20ed7
royneal <royneal@gmail.com>
2018-11-06 19:19:34 -0500

    changed client to run continuosly rather than one shot

--- src/client.cpp
+++ src/client.cpp
@@ -20,13 +20,13 @@ int main(int argc, char **argv) {
     ROS_ERROR("Error: no imput arguments  ");
     return -1;
   }
-
   ros::NodeHandle nh;
   ros::ServiceClient client =
   nh.serviceClient<beginner_tutorials::message_rate>
   ("message_rate");  // register service client
-
+  ros::Rate loop_rate(10);
   beginner_tutorials::message_rate srv;  // create a service object
+  while (ros::ok) {
   srv.request.rate = std::stoi(argv[1]);  // read command line argument
   ROS_DEBUG_STREAM("DEBUG MESSAGE CHECK ARG VALUE:" << srv.request.rate);
   if (client.call(srv)) {  // check if service request was successfull
@@ -36,6 +36,8 @@ int main(int argc, char **argv) {
     ROS_FATAL("Failed to call service message_rate");
     return 1;
   }
-
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
   return 0;
 }
