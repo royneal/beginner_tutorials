d7a344c7051f2cc6b997e68d20fdab8608924fac
royneal <royneal@gmail.com>
2018-11-12 15:49:14 -0500

    wrote a test for the service request change message_rate

--- test/my_packg_test.cpp
+++ test/my_packg_test.cpp
@@ -1,25 +1,28 @@
 // Bring in my package's API, which is what I'm testing
-#include "beginner_tutorials/publisher.h"
-#include "beginner_tutorials/subscriber.h"
+
 // Bring in gtest
 #include <gtest/gtest.h>
-
+#include "ros/ros.h"
+#include "beginner_tutorials/publisher.h"
+#include "beginner_tutorials/subscriber.h"
 // Declare a test
 TEST(TestSuite, testCase1)
 {
-//<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
+  ros::NodeHandle nh;
+  nh.serviceClient<beginner_tutorials::message_rate>
+  ("message_rate");  // register service client
+  beginner_tutorials::message_rate srv;  // create a service object
+  srv.request.rate = 50;  // set service request value
+  int resp=50;
+
+  EXPECT_EQ(1, srv.response.oldrate); // service responds with old rate which is 1 hz
+
 }
 
-// Declare another test
-TEST(TestSuite, testCase2)
-{
-//<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
-}
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "tester");
-  ros::NodeHandle nh;
+  ros::init(argc, argv, "mypackagetest");
   return RUN_ALL_TESTS();
 }
