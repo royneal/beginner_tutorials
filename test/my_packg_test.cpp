443af1c7e4de7388e46ed877d4c46fca6f8ccc4d
royneal <royneal@gmail.com>
2018-11-12 16:34:40 -0500

    added service call to test case in  my_package_test.cpp

--- test/my_packg_test.cpp
+++ test/my_packg_test.cpp
@@ -9,12 +9,12 @@
 TEST(TestSuite, testCase1)
 {
   ros::NodeHandle nh;
-  nh.serviceClient<beginner_tutorials::message_rate>
+  ros::service client = nh.serviceClient<beginner_tutorials::message_rate>
   ("message_rate");  // register service client
   beginner_tutorials::message_rate srv;  // create a service object
   srv.request.rate = 50;  // set service request value
   int resp=50;
-
+  client.call(srv);
   EXPECT_EQ(1, srv.response.oldrate); // service responds with old rate which is 1 hz
 
 }
